f21e34ef4161e5b9d55feb6cae566544a6d059e8
EdiiA <edison.alushaj3@gmail.com>
2024-03-03 22:44:11 +0100

    Home page
    Krijimi i html dhe css file te home page

---
+++ home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    <header>
+        <h2>go.arch</h2>
+
+        <p id="number" style="display: flex; justify-content: center; align-items: center;">(+383) 48 800 450</p>
+        
+        <ul class="nav-ul">
+            <li>HOME</li>
+            <li>ABOUT US</li>
+            <li>PROJECT</li>
+            <li>BLOG</li>
+            <li>CONTACT</li>
+        </ul>
+    </header>
+
+    <main>
+        <div class="slider-main">
+            <div class="slider">
+                <div class="slider-container">
+                    <div><img src="img/R2.jpg" alt=""></div>
+                    <div><img src="img/R7.2.jpg" alt=""></div>
+                    <div><img src="img/unnamed (1).jpg" alt=""></div>
+                </div>
+
+                <div class="prev">&#10094;</div>
+                <div class="next">&#10095;</div>
+            </div>
+        </div>
+
+        
+    </main>
+</body>
+</html>
