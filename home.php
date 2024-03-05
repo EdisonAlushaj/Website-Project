03a273164d67e82070920e59eeb0b136ad6dd02f
EdiiA <edison.alushaj3@gmail.com>
2024-03-05 18:59:27 +0100

    Cover design
    Rregullimi i dizajnit te cover.

--- home.php
+++ home.php
@@ -10,7 +10,7 @@
     <header>
         <h2>go.arch</h2>
 
-        <p id="number" style="display: flex; justify-content: center; align-items: center;">(+383) 48 800 450</p>
+        <p id="number" style="display: flex; justify-content: center; align-items: center; margin-right: 18em;">(+383) 48 800 450</p>
         
         <ul class="nav-ul">
             <li><a href="#">HOME</a></li>
@@ -33,18 +33,55 @@
             <div class="next">&#10095;</div>
         </div> -->
 
-        <div class="slider">    
+        <div class="cover">
+            <div class="slider">    
                 <div class="slider-container">
                     <div class="slides">
-                        <div class="slide"><img src="img/R2.jpg" alt="Image 1"></div>
-                        <div class="slide"><img src="img/R7.2.jpg" alt="Image 2"></div>
-                        <div class="slide"><img src="img/unnamed (1).jpg" alt="Image 3"></div>
+                        <div class="slide">
+                            <img src="img/R7.2.jpg" alt="Image 1">
+
+                            <div class="slide-txt">
+                                <p>Flat 53</p>
+                                <p>Ipsum cillum quis eu eu culpa. Ullamco consectetur incididunt eiusmod proident ex aliqua fugiat exercitation est.</p>
+                            </div>
+                        </div>
+                        <div class="slide">
+                            <img src="img/R2.jpg" alt="Image 2">
+                        
+                            <div class="slide-txt">
+                                <p>Flat 53</p>
+                                <p>Ipsum cillum quis eu eu culpa. Ullamco consectetur incididunt eiusmod proident ex aliqua fugiat exercitation est.</p>
+                            </div>
+                        </div>
+                        <div class="slide">
+                            <img src="img/unnamed (1).jpg" alt="Image 3">
+                        
+                            <div class="slide-txt">
+                                <p>Flat 53</p>
+                                <p>Ipsum cillum quis eu eu culpa. Ullamco consectetur incididunt eiusmod proident ex aliqua fugiat exercitation est.</p>
+                            </div>
+                        </div>
                     </div>
                 
                     <div class="prev">&#10094; PREV</div>
                     <div class="next">NEXT &#10095;</div>
                 </div>  
             </div>
+
+            <div class="cover-num">
+                <p id="num"></p>
+            </div>
+
+            <h1 id="transparent-txt">arch</h1>
+
+            <div class="cover-social">
+                <!-- <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul> -->
+            </div>
+        </div>
     </main>
 
     <script>
@@ -74,6 +111,8 @@
             const translateValue = -currentIndex * 100 + '%';
             slides.style.transform = `translateX(${translateValue})`;
         }
+
+        document.getElementById("num").innerHTML = "0" + (currentIndex + 1);
     </script>
 </body>
 </html>
